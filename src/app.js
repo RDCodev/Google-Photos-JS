75bba604a5bc396f8fea1a38ab263e4eeaadebec
RDCodev <rdcode.dev@gmail.com>
2023-09-05 22:09:31 -0500

    refactor: Restructuration functions to classes on google auth flow & google download image

--- src/app.js
+++ src/app.js
@@ -1,84 +1,22 @@
 import 'dotenv/config'
 import { pinoConfig } from './config/pino.config.js'
-import { urlAuthCodeRequest, googlePhotosAuthConfig } from './config/creds.config.js'
-import { authTokenRequest } from './auth/token.request.js'
-import http from 'http'
-import https from 'https'
-import logger from 'pino'
-import open from 'open'
-import fs from 'fs'
+import { GooglePhotosService, GoogleService } from './config/creds.config.js'
+import { dirname, join } from 'path'
+import { fileURLToPath } from 'url'
 
 function main(){
-
-  if(fs.existsSync('token.json')){
-    
-    let data = ""
-    const token = JSON.parse(fs.readFileSync('./token.json'))
-    const options = {
-      headers: {
-        'Content-type': 'application/json',
-        'Authorization': `Bearer ${token.access_token}`
-      }
-    }
-
-    const req = https.get('https://photoslibrary.googleapis.com/v1/mediaItems',options, (res) => {
-
-      res.on('data', chunk => {
-        data += chunk
-      })
-
-      res.on('end', () => {
-        const { mediaItems } = JSON.parse(data)    
-        let image = []
-        const reqImage = https.get(`${mediaItems[0].baseUrl}=d`, options, (res) => {
-          res.on('data', chunk => {            
-            image = [...image, chunk]
-          })          
-
-          res.on('end', () => {                      
-            fs.writeFileSync(`${mediaItems[0].filename}`, Buffer.concat([...image]))
-          })
-        })
-
-
-        reqImage.end()
-      })
-
-      res.on('error', err => {
-        console.log(err)
-      })
-    })
-
-    req.end()
-
-    return
-  }
   
-  open(urlAuthCodeRequest, { app: 'google chrome' })
+  const googleService = new GoogleService()
+  const googlePhotos = new GooglePhotosService()
   
-  const server = http.createServer((req, res) => { 
-    res.end()
-  })
-  
-  server.on('request', (req, res) => {
-    const url = new URL(req.url, `http://${req.headers.host}`)
-    let codeAuthorization = null  
-  
-    if(url.searchParams.has('code')){
-      codeAuthorization = url.searchParams.get('code')      
-      authTokenRequest(googlePhotosAuthConfig(codeAuthorization))    
-    }  
-  
-    if(url.searchParams.has('error')){
-      return
-    }
-  })
-  
-  server.on('listening', () => {
-    console.log(`Listening on port ${process.env.PORT}`)
-  })
-  
-  server.listen(process.env.PORT | 8080)
+  googleService.loadCredentials(join(
+    dirname(fileURLToPath(import.meta.url)), './auth/credentials.json'))
+
+  googleService.requestCredentials(['https://www.googleapis.com/auth/photoslibrary'])
+  .then(
+    token => googlePhotos.downloadImages(token)
+  )
+
 }
 
 main()
